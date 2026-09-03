751798370cb8a97d820ac4aa313ab13e27419dad
Gemma Serenity <gorokhoff.gemma@gmail.com>
2026-09-03 11:09:48 -0700

    Add Cloudflare Pages security headers

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,4 +8,5 @@ await Promise.all([
   cp(new URL("../public/og.png", import.meta.url), new URL("../dist/og.png", import.meta.url)),
   cp(new URL("../public/gemma-serenity.jpg", import.meta.url), new URL("../dist/gemma-serenity.jpg", import.meta.url)),
   cp(new URL("../public/sascha-gorokhoff.jpg", import.meta.url), new URL("../dist/sascha-gorokhoff.jpg", import.meta.url)),
+  cp(new URL("../public/_headers", import.meta.url), new URL("../dist/_headers", import.meta.url)),
 ]);
